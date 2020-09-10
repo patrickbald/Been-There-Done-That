81c1a1678db4d2fb92bd90290b3dc5d5490617aa
Patrick Bald <pbald@nd.edu>
2020-09-10 17:02:28 -0400

    nav bar

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Header from '../../../React/udemy-class/final_course_projects/contactmanager/src/components/layout/Header';
+
+const NavBar = props => {
+    return(
+        <nav className="navbar">
+            
+        </nav>
+    );
+}
+
+export default Header;
