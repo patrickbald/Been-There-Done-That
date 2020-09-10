cbbfaef161962ece095718ac010e2131872a8b08
Patrick Bald <pbald@nd.edu>
2020-09-10 16:32:09 -0400

    basic trip setup

---
+++ src/components/Trip.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Trip extends Component(){
+    render(){
+        return(
+            <div class="trip">
+                
+            </div>
+        )
+    }
+}
+
+export default Trip;
