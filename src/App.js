a56fe55487e02702ed2642001a19f15f068ee87c
Patrick Bald <pbald@nd.edu>
2020-09-13 20:55:50 -0400

    adding state to app, rendering down into trip components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Feed from "./components/Feed";
 import Header from "./components/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <Header branding="been there, done that" />
-      <br></br>
-      <div className="container">
-        <Feed></Feed>
+class App extends Component {
+
+  state = {
+    trips: [
+      {
+          "name" : "Patrick Bald",
+          "Location" : "Paris",
+          "Rating" : 5
+      },
+      {
+          "name" : "Eleanor",
+          "Location" : "Rome",
+          "Rating" : 4.5
+      }
+    ]
+  };
+
+  render(){
+    return (
+      <div className="App">
+        <Header branding="been there, done that" />
+        <br></br>
+        <div className="container">
+          <Feed trips_array={this.state.trips}></Feed>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,19 +3,20 @@ import Trip from './Trip';
 
 class Feed extends Component {
 
-    // state = {
-    //     trips: [
-    //         {
-
-    //         }
-    //     ]
+    // constructor(props){
+    //     super(props);
     // }
 
+
     render(){
-        // const {trips} = this.state.trips;
+        const { trips } = this.props;
+        console.log(trips);
+
         return(
             <React.Fragment>
-                <Trip name="Pat" place="Paris" rating="5"></Trip>
+                {this.props.trips_array.map((trip) => {
+                    return <Trip name={trip.name} place={trip.place} rating={trip.rating}></Trip>
+                })}
             </React.Fragment>
         );
     }
