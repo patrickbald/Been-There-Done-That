de2f2d6fa67595e08a638e5a14c99873e87b9bec
Eleanor Kinyon <eleanorkinyon@Eleanors-MacBook-Pro.local>
2020-09-16 10:45:52 -0400

    fix filterting

--- src/App.js
+++ src/App.js
@@ -3,74 +3,56 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Feed from "./components/Feed";
 import Header from "./components/Header";
-import axios from 'axios'
+import axios from "axios";
 
 class App extends Component {
-
   state = {
-    trips: [
-      {
-        id: 1,
-        name: "Patrick Bald",
-        place: "Paris",
-        rating: 5,
-        photo: "../public/images/paris.jpeg",
-      },
-      {
-        id: 2,
-        name: "Eleanor",
-        place: "Rome",
-        rating: 4,
-        photo: "../public/images/rome.jpg",
-      },
-    ],
-    trips2: [
-      {
-        id: 2,
-        name: "test",
-        place: "Rome",
-        rating: 4,
-        photo: "../public/images/rome.jpg",
-      }
-    ],
-    isLoading: true
+    trips2: [],
+    isLoading: true,
   };
 
   //this.state.filteredTrips = this.state.trips;
   filterState = (search) => {
     console.log(search);
     console.log("filter");
-    const currentTrips = this.state.trips;
+    const currentTrips = this.state.trips2;
+    console.log(currentTrips);
     const filteredTrips = currentTrips.filter(
-      (trip) => trip.place.includes(search) || trip.name.includes(search)
+      (trip) => trip.location.includes(search) || trip.name.includes(search)
     );
     //  currentTrips.forEach((trips) => console.log(trips.name, trips.place));
     //filteredTrips.append()
     //return filteredTrips;
-    this.setState({ trips: filteredTrips });
+    this.setState({ trips2: filteredTrips });
     // this.setState({ filteredTrips: searchedTrips });
 
     //  console.log(search, currentTrips, filteredTrips);
   };
 
-  componentDidMount(){
-    console.log("component did mount enter")
-    axios.get('trips.json')
-    // .then(response => response.data)
-    // .then(response => console.log(response.data.trips))
-    .then(response => this.setState({
-      trips2: response.data.trips,
-      isLoading: false
-    }))
-  };
+  componentDidMount() {
+    console.log("component did mount enter");
+    axios
+      .get("trips.json")
+      // .then(response => response.data)
+      // .then(response => console.log(response.data.trips))
+      .then((response) =>
+        this.setState({
+          trips2: response.data.trips,
+          isLoading: false,
+        })
+      );
+  }
 
   render() {
     return (
       <div className="App">
-        <Header branding="been there, done that" onSearch={this.filterState} />
+        <Header onSearch={this.filterState} />
         <br></br>
         <div className="container">
-          <Feed trips_array={this.state.trips2} loading={this.state.isLoading}></Feed>
+          <Feed
+            trips_array={this.state.trips2}
+            loading={this.state.isLoading}
+          ></Feed>
         </div>
       </div>
     );
