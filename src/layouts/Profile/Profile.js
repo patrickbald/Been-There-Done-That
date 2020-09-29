41c17d8d23aa8742de4575a299c930c19b131fb8
Eleanor Kinyon <eleanorkinyon@eleanors-mbp.dhcp.nd.edu>
2020-09-29 13:32:07 -0400

    profile layout

--- src/layouts/Profile/Profile.js
+++ src/layouts/Profile/Profile.js
@@ -5,6 +5,8 @@ import Header from "../../components/Header/Header";
 import Image from "react-bootstrap/Image";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 
 function Profile() {
   return (
@@ -12,23 +14,33 @@ function Profile() {
       <Header />
       <br></br>
       <div className="container">
-        <h1>Christopher Collard</h1>
-        <Row>
+        <h1 style={{ float: "left" }}>Christopher Collard</h1>
+        <br></br>
+
+        <Row style={{ float: "left" }}>
           <Col xs={6} md={4}>
             <Image
               width={271}
               height={290}
-              src="images/pfp.jpg"
+              src="images/man.jpg"
               roundedCircle
             />
           </Col>
           <Col xs={6} md={4}>
-            <Image src="holder.js/171x180" roundedCircle />
+            <Card style={{ width: "45rem" }}>
+              <Card.Header className="font-weight-bold">@ccoll</Card.Header>
+              <ListGroup variant="flush">
+                <ListGroup.Item>Most recent trip: Tokyo, Japan</ListGroup.Item>
+                <ListGroup.Item style={{ height: "12rem" }}>
+                  grew up in Portland, currently living in London, love
+                  photography
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
           </Col>
-          <Col xs={6} md={4}>
-            <Image src="holder.js/171x180" thumbnail />
-          </Col>
+          <Col xs={6} md={4}></Col>
         </Row>
+        <br></br>
       </div>
     </div>
   );
