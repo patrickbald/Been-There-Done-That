f880fef92969805b9541c0180e76fae9746e4733
Eleanor Kinyon <eleanorkinyon@Eleanors-MacBook-Pro.local>
2020-09-29 00:23:30 -0400

    routing

---
+++ src/layouts/Profile/Profile.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Feed from "../../components/Feed/Feed";
+import Header from "../../components/Header/Header";
+import axios from "axios";
+
+function Profile() {
+  return (
+    <div className="Profile">
+      <Header />
+      <br></br>
+      <div className="container">
+        <h1>test</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
