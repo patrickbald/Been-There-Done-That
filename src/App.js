8c0b4711a21def5c8e1efec7d0a9632afc999282
Eleanor Kinyon <eleanorkinyon@Eleanors-MacBook-Pro.local>
2020-09-30 20:15:18 -0400

    remove common folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import "../App.css";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Feed from "../components/Feed/Feed";
-import Header from "../components/Header/Header";
-import Home from "../layouts/Home/Home";
-import Profile from "../layouts/Profile/Profile";
-import Add from "../layouts/Add/Add";
-import Plan from "../layouts/Plan/Plan";
+import Feed from "./components/Feed/Feed";
+import Header from "./components/Header/Header";
+import Home from "./layouts/Home/Home";
+import Profile from "./layouts/Profile/Profile";
+import Add from "./layouts/Add/Add";
+import Plan from "./layouts/Plan/Plan";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Parse from "parse";
