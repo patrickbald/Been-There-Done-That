a915ba8d344f636340fc94fcd89ca826ca369aee
Patrick Bald <pbald@nd.edu>
2020-10-06 16:29:21 -0400

    changing back4app info

--- src/common/environments.js
+++ src/common/environments.js
@@ -1,5 +1,5 @@
 module.exports = {
-  APPLICATION_ID: "935jUrDeiqMwTRc51kZJBFU2zbU0iDKiDDxvsV9N",
-  JAVASCRIPT_KEY: "h13fRfhYOFR5qw6wi68XdnRXgqmpehi8QwIMRWZO",
+  APPLICATION_ID: "ijUG6XS4uAfubouujJEYf3EpWRvwkdD8hIo6qX1B",
+  JAVASCRIPT_KEY: "g0tXbCa5Cz38nxifpvcNOzNCzv2s4pYV83EM9818",
   SERVER_URL: "https://parseapi.back4app.com/",
 };
