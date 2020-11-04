90873c5f8b52d71af7e253b40bcea44c622150a8
Patrick Bald <pbald@nd.edu>
2020-11-04 01:26:39 -0500

    working on implementing algolia

--- src/layouts/Home/Home.js
+++ src/layouts/Home/Home.js
@@ -5,6 +5,7 @@ import Feed from "../../components/Feed/Feed";
 import Header from "../../components/Header/Header";
 import Search from "../../components/Search/Search";
 import Parse from "parse";
+import { InstantSearch, SearchBox } from 'react-instantSearch';
 
 class Home extends Component {
   constructor() {
@@ -94,13 +95,99 @@ class Home extends Component {
     });
   };
 
+  // Algolia Functionality
+
+  reindexAlgoliaData = () => {
+    const algoliasearch = require('algoliasearch');
+    const client = algoliasearch('0D2QURHTOH', '35140dd4e563460722777dbba81bfa75');
+    const index = client.initIndex('prod_been_there_done_that');
+
+    let objectsToIndex = [];
+
+    const trip_query = new Parse.Query('Trip');
+    trip_query.find({
+      success(trips) {
+        // prepare objects to index 
+        objectsToIndex = trips.map(trip => {
+          // convert to regular key/value JavaScript object
+          trip = trip.toJSON();
+          // Specify Algolia's objectID with the Parse.Object unique ID
+          trip.objectID = trip.objectId;
+          return trip;
+        });
+        index.replaceAllObjects(objectsToIndex, { safe: true }).then(() => {
+          console.log('Done re-indexing Trip data');
+        });
+      },
+      error(err) {
+        throw err;
+      }
+    })
+
+    objectsToIndex = [];
+    const usr_query = new Parse.Query('User');
+    usr_query.find({
+      success(users) {
+        objectsToIndex = users.map(usr => {
+          usr = usr.toJSON();
+          usr.objectID = usr.objectId;
+          return usr;
+        });
+        index.replaceAllObjects(objectsToIndex, { safe: true }).then(() => {
+          console.log('Done re-indexing User data');
+        });
+      },
+      error(err) {
+        throw err;
+      }
+    })
+
+    objectsToIndex = [];
+    const cntry_query = new Parse.Query('Country');
+    cntry_query.find({
+      success(cntrys) {
+        objectsToIndex = cntrys.map(c => {
+          c = c.toJSON();
+          c.objectID = c.objectId;
+          return c;
+        });
+        index.replaceAllObjects(objectsToIndex, { safe: true }).then(() => {
+          console.log('Done re-indexing Country data');
+        });
+      },
+      error(err) {
+        throw err;
+      }
+    })
+
+  }
+
+  updateAlgoliaData = () => {
+    const algoliasearch = require('algoliasearch');
+    const client = algoliasearch('0D2QURHTOH', '35140dd4e563460722777dbba81bfa75');
+    const index = client.initIndex('prod_been_there_done_that');
+
+    Parse.Cloud.afterSave('Trip', ({object}) => {
+      // Convert Parse.Object to JSON
+      const objectToSave = object.toJSON();
+      // Specify Algolia's objectID with the Parse.Object unique ID
+      objectToSave.objectID = objectToSave.objectId;
+      // Add or update object
+      index.saveObject(objectToSave).then(() => {
+        console.log('Parse<>Algolia object saved');
+      });
+    });
+  }
+
   render() {
     return (
       <div className="Home">
         <Header />
         <br></br>
         <div className="container">
-          <Search onSearch={this.filter} />
+          <InstantSearch indexName="prod_been_there_done_that" searchClient={searchClient} >
+            <SearchBox searchAsYouType={true}/>
+          </InstantSearch>
           <br/>
           <Feed
             trips_array={this.state.rendered}
